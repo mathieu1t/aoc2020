93fce3add4e2de60c79d44d7889033f63b3e3559
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2019-12-10 17:50:36 +0100

    jour7p2 de merde

--- src/main/java/fr/insee/adventofcode/days/Day07.java
+++ src/main/java/fr/insee/adventofcode/days/Day07.java
@@ -12,24 +12,24 @@ public class Day07 extends Day<Integer> {
 	@Override
 	public String part1(String filepath, Object... params) {
 		puzzle = Utils.getTabEntier(filepath, ",");
-		Integer sauv[] = Arrays.copyOf(puzzle, puzzle.length);
+		Integer[] sauv = Arrays.copyOf(puzzle, puzzle.length);
 		List<Integer> signals = new ArrayList<>();
 		List<Integer[]> reglages = reglagesPossibles(0,4);
 		for (Integer[] i : reglages) {
 			int entree = 0;
-			IntCode intCodeA = new IntCode(puzzle, i[0],entree);
+			IntCode intCodeA = new IntCode(puzzle, i[0],entree, 0);
 			entree = intCodeA.lancer();
 			puzzle = Arrays.copyOf(sauv, sauv.length);
-			IntCode intCodeB = new IntCode(puzzle, i[1],entree);
+			IntCode intCodeB = new IntCode(puzzle, i[1],entree, 0);
 			entree = intCodeB.lancer();
 			puzzle = Arrays.copyOf(sauv, sauv.length);
-			IntCode intCodeC = new IntCode(puzzle, i[2],entree);
+			IntCode intCodeC = new IntCode(puzzle, i[2],entree, 0);
 			entree = intCodeC.lancer();
 			puzzle = Arrays.copyOf(sauv, sauv.length);
-			IntCode intCodeD = new IntCode(puzzle, i[3],entree);
+			IntCode intCodeD = new IntCode(puzzle, i[3],entree, 0);
 			entree = intCodeD.lancer();
 			puzzle = Arrays.copyOf(sauv, sauv.length);
-			IntCode intCodeE = new IntCode(puzzle, i[4],entree);
+			IntCode intCodeE = new IntCode(puzzle, i[4],entree, 0);
 			entree = intCodeE.lancer();
 			puzzle = Arrays.copyOf(sauv, sauv.length);
 			signals.add(entree);
@@ -42,45 +42,33 @@ public class Day07 extends Day<Integer> {
 	@Override
 	public String part2(String filepath, Object... params) {
 		puzzle = Utils.getTabEntier(filepath, ",");
-		Integer sauv[] = Arrays.copyOf(puzzle, puzzle.length);
 		List<Integer> signals = new ArrayList<>();
 		List<Integer[]> reglages = reglagesPossibles(5, 9);
-		for (Integer[] i : reglages) {
-			puzzle = Arrays.copyOf(puzzle, puzzle.length);
+		for (Integer[] i : reglages) {	    
 			int entree = 0;
-			IntCode intCodeA = new IntCode(puzzle, i[0],entree);
-			entree = intCodeA.lancer();
-			puzzle = Arrays.copyOf(sauv, sauv.length);
-			IntCode intCodeB = new IntCode(puzzle, i[1],entree);
-			entree = intCodeB.lancer();
-			puzzle = Arrays.copyOf(sauv, sauv.length);
-			IntCode intCodeC = new IntCode(puzzle, i[2],entree);
-			entree = intCodeC.lancer();
-			puzzle = Arrays.copyOf(sauv, sauv.length);
-			IntCode intCodeD = new IntCode(puzzle, i[3],entree);
-			entree = intCodeD.lancer();
-			puzzle = Arrays.copyOf(sauv, sauv.length);
-			IntCode intCodeE = new IntCode(puzzle, i[4],entree);
-			entree = intCodeE.lancer();
-			puzzle = Arrays.copyOf(sauv, sauv.length);
+			IntCode intCodeA = new IntCode(puzzle, i[0],entree, 0);
+			int sortieA = intCodeA.lancer();
+			IntCode intCodeB = new IntCode(puzzle, i[1],sortieA, intCodeA.getPosition());
+			int sortieB = intCodeB.lancer();
+			IntCode intCodeC = new IntCode(puzzle, i[2],sortieB, intCodeB.getPosition());
+			int sortieC = intCodeC.lancer();
+			IntCode intCodeD = new IntCode(puzzle, i[3],sortieC, intCodeC.getPosition());
+			int sortieD = intCodeD.lancer();
+			IntCode intCodeE = new IntCode(puzzle, i[4],sortieD, intCodeD.getPosition());
+			int sortieE = intCodeE.lancer();
 			while (!intCodeE.isStop()) {
-				intCodeA = new IntCode(puzzle, i[0],entree);
-				entree = intCodeA.lancer();
-				puzzle = Arrays.copyOf(sauv, sauv.length);
-				intCodeB = new IntCode(puzzle, i[1],entree);
-				entree = intCodeB.lancer();
-				puzzle = Arrays.copyOf(sauv, sauv.length);
-				intCodeC = new IntCode(puzzle, i[2],entree);
-				entree = intCodeC.lancer();
-				puzzle = Arrays.copyOf(sauv, sauv.length);
-				intCodeD = new IntCode(puzzle, i[3],entree);
-				entree = intCodeD.lancer();
-				puzzle = Arrays.copyOf(sauv, sauv.length);
-				intCodeE = new IntCode(puzzle, i[4],entree);
-				entree = intCodeE.lancer();
-				puzzle = Arrays.copyOf(sauv, sauv.length);
+				intCodeA = new IntCode(puzzle, null,sortieE, intCodeE.getPosition());
+				sortieA = intCodeA.lancer();
+				intCodeB = new IntCode(puzzle, null,sortieA, intCodeA.getPosition());
+				sortieB = intCodeB.lancer();
+				intCodeC = new IntCode(puzzle, null,sortieB, intCodeB.getPosition());
+				sortieC = intCodeC.lancer();
+				intCodeD = new IntCode(puzzle, null,sortieC, intCodeC.getPosition());
+				sortieD = intCodeD.lancer();
+				intCodeE = new IntCode(puzzle, null,sortieD, intCodeD.getPosition());
+				sortieE = intCodeE.lancer();
 			}
-			signals.add(entree);
+			signals.add(sortieD);
 		}
 
 		int retour = signals.stream().mapToInt(x -> x).max().orElse(0);
--- src/main/java/fr/insee/adventofcode/model/IntCode.java
+++ src/main/java/fr/insee/adventofcode/model/IntCode.java
@@ -2,125 +2,142 @@ package fr.insee.adventofcode.model;
 
 public class IntCode {
 
-	private boolean stop = false;
-	private Integer[] tab;
-	private int entree1 = 0;
-	private int entree2 = 0;
-	private int position = 0;
+    private boolean stop = false;
+    private Integer[] tab;
+    private Integer entree1 = 0;
+    private Integer entree2 = 0;
+    private int position;
 
-	public IntCode(Integer[] tab, int entree1, int entree2) {
-		this.tab = tab;
-		this.entree1 = entree1;
-		this.entree2 = entree2;
+    public IntCode(Integer[] tab, Integer entree1, Integer entree2, int position) {
+	this.tab = tab;
+	this.entree1 = entree1;
+	this.entree2 = entree2;
+	this.position = position;
+    }
+
+    public String getOpcode(String s) {
+	String opcode = s;
+	if (opcode.length() < 2) {
+	    opcode = "0" + opcode;
+	} else if (opcode.length() > 2) {
+	    opcode = opcode.substring(opcode.length() - 2);
 	}
+	return opcode;
+    }
 
-	public String getOpcode(String s) {
-		String opcode = s;
-		if (opcode.length() < 2) {
-			opcode = "0" + opcode;
-		} else if (opcode.length() > 2) {
-			opcode = opcode.substring(opcode.length() - 2);
-		}
-		return opcode;
+    public Integer[] getModes(String s) {
+	if (s.length() >= 2) {
+	    int nbModes = s.length() - 2;
+	    Integer[] modes = new Integer[nbModes];
+	    for (int n = 0; n < nbModes; n++) {
+		modes[nbModes - n - 1] = Character.getNumericValue(String.valueOf(tab[position]).charAt(n));
+	    }
+
+	    return modes;
+	} else {
+	    return new Integer[0];
 	}
+    }
 
-	public Integer[] getModes(String s) {
-		if (s.length() >= 2) {
-			int nbModes = s.length() - 2;
-			Integer[] modes = new Integer[nbModes];
-			for (int n = 0; n < nbModes; n++) {
-				modes[nbModes - n - 1] = Character.getNumericValue(String.valueOf(tab[position]).charAt(n));
-			}
-
-			return modes;
+    public int lancer() {
+	int sortie = 0;
+	String opcode = getOpcode(String.valueOf(tab[position]));
+	boolean changeInput = false;
+	while (!"99".equals(opcode)) {
+	    Integer[] modes = getModes(String.valueOf(tab[position]));
+	    switch (opcode) {
+	    case "01":
+		tab[tab[position + 3]] = val(1, position, modes, tab) + val(2, position, modes, tab);
+		position = position + 4;
+		break;
+	    case "02":
+		tab[tab[position + 3]] = val(1, position, modes, tab) * val(2, position, modes, tab);
+		position = position + 4;
+		break;
+	    case "03":
+		if (!changeInput && entree1 != null) {
+		    tab[tab[position + 1]] = entree1; // input
+		    changeInput = true;
 		} else {
-			return new Integer[0];
+		    tab[tab[position + 1]] = entree2; // secondInput
 		}
-	}
-
-	public int lancer() {
-		String opcode = getOpcode(String.valueOf(tab[position]));
-		boolean changeInput = false;
-		while (!"99".equals(opcode)) {
-			Integer[] modes = getModes(String.valueOf(tab[position]));
-			switch (opcode) {
-			case "01":
-				tab[tab[position + 3]] = val(1, position, modes, tab) + val(2, position, modes, tab);
-				position = position + 4;
-				break;
-			case "02":
-				tab[tab[position + 3]] = val(1, position, modes, tab) * val(2, position, modes, tab);
-				position = position + 4;
-				break;
-			case "03":
-				if (!changeInput) {
-					tab[tab[position + 1]] = entree1; // input
-					changeInput = true;
-				} else {
-					tab[tab[position + 1]] = entree2; // secondInput
-				}
-				position = position + 2;
-				break;
-			case "04":
-				int sortie = val(1, position, modes, tab); // output
-				position = position + 2;
-				return sortie;
-			case "05":
-				if (val(1, position, modes, tab) != 0) {
-					tab[position] = val(2, position, modes, tab);
-					position = tab[position];
-				} else {
-					position = position + 3;
-				}
-				break;
-			case "06":
-				if (val(1, position, modes, tab) == 0) {
-					tab[position] = val(2, position, modes, tab);
-					position = tab[position];
-				} else {
-					position = position + 3;
-				}
-				break;
-			case "07":
-				tab[tab[position + 3]] = val(1, position, modes, tab) < val(2, position, modes, tab) ? 1 : 0;
-				position = position + 4;
-				break;
-			case "08":
-				tab[tab[position + 3]] = val(1, position, modes, tab) == val(2, position, modes, tab) ? 1 : 0;
-				position = position + 4;
-				break;
-			}
-			opcode = getOpcode(String.valueOf(tab[position]));
+		position = position + 2;
+		break;
+	    case "04":
+		sortie = val(1, position, modes, tab); // output
+		position = position + 2;
+		return sortie;
+	    case "05":
+		if (val(1, position, modes, tab) != 0) {
+		    //tab[position] = val(2, position, modes, tab);
+		    position = tab[position];
+		} else {
+		    position = position + 3;
 		}
-		setStop(true);
-		return 0;
+		break;
+	    case "06":
+		if (val(1, position, modes, tab) == 0) {
+		    //tab[position] = val(2, position, modes, tab);
+		    position = tab[position];
+		} else {
+		    position = position + 3;
+		}
+		break;
+	    case "07":
+		tab[tab[position + 3]] = val(1, position, modes, tab) < val(2, position, modes, tab) ? 1 : 0;
+		position = position + 4;
+		break;
+	    case "08":
+		tab[tab[position + 3]] = val(1, position, modes, tab) == val(2, position, modes, tab) ? 1 : 0;
+		position = position + 4;
+		break;
+	    }
+	    opcode = getOpcode(String.valueOf(tab[position]));
 	}
+	setStop(true);
+	return sortie;
+    }
 
-	/**
-	 * @return the tab
-	 */
-	public Integer[] getTab() {
-		return tab;
-	}
+    /**
+     * @return the tab
+     */
+    public Integer[] getTab() {
+	return tab;
+    }
 
-	/**
-	 * @param tab the tab to set
-	 */
-	public void setTab(Integer[] tab) {
-		this.tab = tab;
-	}
+    /**
+     * @param tab
+     *            the tab to set
+     */
+    public void setTab(Integer[] tab) {
+	this.tab = tab;
+    }
 
-	public int val(int numParam, int position, Integer[] modes, Integer[] puzzle) {
-		boolean mode = modes.length >= numParam && modes[numParam - 1] == 1;
-		return puzzle[mode ? position + numParam : puzzle[position + numParam]];
-	}
+    public int val(int numParam, int position, Integer[] modes, Integer[] puzzle) {
+	boolean mode = modes.length >= numParam && modes[numParam - 1] == 1;
+	return puzzle[mode ? position + numParam : puzzle[position + numParam]];
+    }
 
-	public boolean isStop() {
-		return stop;
-	}
+    public boolean isStop() {
+	return stop;
+    }
 
-	public void setStop(boolean stop) {
-		this.stop = stop;
-	}
+    public void setStop(boolean stop) {
+	this.stop = stop;
+    }
+
+    /**
+     * @return the position
+     */
+    public int getPosition() {
+        return position;
+    }
+
+    /**
+     * @param position the position to set
+     */
+    public void setPosition(int position) {
+        this.position = position;
+    }
 
 }
