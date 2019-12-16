cdb45cbd826e89fe51bb74f19f040b159b9956fd
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2019-12-16 17:09:17 +0100

    day 14

--- src/main/java/fr/insee/adventofcode/days/Day14.java
+++ src/main/java/fr/insee/adventofcode/days/Day14.java
@@ -1,8 +1,6 @@
 package fr.insee.adventofcode.days;
 
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import fr.insee.adventofcode.model.Reaction;
@@ -12,12 +10,15 @@ public class Day14 extends Day {
 
     @Override
     public String part1(String filepath, Object... params) {
-	Map<String,Reaction> reactions = new TreeMap<>();
-	List<Reaction> liste = Utils.getLignes(filepath).map(r -> new Reaction(r)).collect(Collectors.toList());
-	liste.stream().forEach(r -> reactions.put(r.sortie, r));
-	long countFUEL = reactions.get("FUEL").findCountOre(1, reactions, 0);
+	List<Reaction> reactions = Utils.getLignes(filepath).map(r -> new Reaction(r)).sorted().collect(Collectors.toList());
+	Reaction fuel = findReaction("FUEL", reactions);
+	long countFUEL = fuel.findCountOre(1, reactions);
 	
 	return String.valueOf(countFUEL);
+    }
+
+    private Reaction findReaction(String s, List<Reaction> reactions) {
+	return reactions.stream().filter(x -> x.sortie .equals(s)).findFirst().orElse(null);
     }
 
     @Override
--- src/main/java/fr/insee/adventofcode/model/Reaction.java
+++ src/main/java/fr/insee/adventofcode/model/Reaction.java
@@ -1,16 +1,18 @@
 package fr.insee.adventofcode.model;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.TreeMap;
 
-public class Reaction {
+public class Reaction implements Comparable<Reaction>{
 
     public String sortie;
     public long quantite;
 
-    public TreeMap<String, Long> entrees;
+    public Map<String, Long> entrees;
+    
+    public int complexity;
 
     public Reaction(String r) {
 	String[] es = r.split(" => ");
@@ -19,57 +21,71 @@ public class Reaction {
 	sortie = sort[1];
 	quantite = Long.parseLong(sort[0]);
 
-	entrees = new TreeMap<>();
+	entrees = new HashMap<>();
 	for (String e : ent) {
 	    String[] entree = e.split(" ");
 	    entrees.put(entree[1], Long.parseLong(entree[0]));
 	}
+	complexity = entrees.size();
     }
 
-//    public Map<String, Long> findBase(long quantite, Map<String, Reaction> reactions, Map<String, Long> base) {
-//	for (String x : this.entrees.keySet()) {
-//	    if (x.equals("ORE")) {
-//		if (base.containsKey(sortie)) {
-//		    base.put(sortie,base.get(sortie)+quantite);
-//		} else {
-//		    base.put(sortie,quantite);
-//		}
-//	    } else {
-//		reactions.get(x).findBase(this.entrees.get(x) * (long)Math.ceil((double)quantite / this.quantite), reactions, base);
-//	    }
-//	}
-//	return base;
-//    }
-//
-//    public long findCountOre(long quantite, Map<String, Reaction> reactions) {
-//	long retour = 0;
-//	Map<String, Long> base = findBase(quantite, reactions, new HashMap<String, Long>());
-//	for (Entry<String,Long> entry : base.entrySet()) {
-//	    long qOre = reactions.get(entry.getKey()).entrees.get("ORE").longValue();
-//	    retour += Math.ceil((double)entry.getValue() / reactions.get(entry.getKey()).quantite) * qOre;
-//	}
-//	return retour;
-//    }
-
-    public long findCountOre(long quantite, Map<String, Reaction> reactions, long oreEnTrop) {
-
-	long retour = 0;
-
-	long q = (long) Math.ceil((double) quantite / this.quantite);
-	
+    public Map<String, Long> findBase(long quantite, List<Reaction> reactions, Map<String, Long> base) {
 	for (String x : this.entrees.keySet()) {
 	    if (x.equals("ORE")) {
-		long nbOre = this.entrees.get(x);
-		retour += nbOre * q;
-		oreEnTrop += nbOre * q - (nbOre * quantite / this.quantite);
-		System.out.println(oreEnTrop);
+		if (base.containsKey(sortie)) {
+		    base.put(sortie, base.get(sortie) + quantite);
+		} else {
+		    base.put(sortie, quantite);
+		}
 	    } else {
-		retour += reactions.get(x).findCountOre(
-			this.entrees.get(x) * (long) Math.ceil((double) quantite / this.quantite), reactions, oreEnTrop);
+		findReaction(x,reactions).findBase(this.entrees.get(x) * (long) Math.ceil((double) quantite / this.quantite), reactions, base);
 	    }
 	}
+	return base;
+    }
 
+    public long findCountOre(long quantite, List<Reaction> reactions) {
+	long retour = 0;
+	Map<String, Long> base = findBase(quantite, reactions, new HashMap<String, Long>());
+	for (Entry<String, Long> entry : base.entrySet()) {
+	    long qOre = findReaction(entry.getKey(), reactions).entrees.get("ORE").longValue();
+	    retour += Math.ceil((double) entry.getValue() / findReaction(entry.getKey(),reactions).quantite) * qOre;
+	}
 	return retour;
     }
+
+    @Override
+    public int compareTo(Reaction o) {
+	if (this.complexity > o.complexity) return -1;
+	if (this.complexity < o.complexity) return 1;
+	return 0;
+    }
+    
+    private Reaction findReaction(String s, List<Reaction> reactions) {
+	return reactions.stream().filter(x -> x.sortie .equals(s)).findFirst().orElse(null);
+    }
+
+    // public long findCountOre(long quantite, Map<String, Reaction> reactions, long
+    // oreEnTrop) {
+    //
+    // long retour = 0;
+    //
+    // long q = (long) Math.ceil((double) quantite / this.quantite);
+    //
+    // for (String x : this.entrees.keySet()) {
+    // if (x.equals("ORE")) {
+    // long nbOre = this.entrees.get(x);
+    // retour += nbOre * q;
+    // oreEnTrop += nbOre * q - (nbOre * quantite / this.quantite);
+    // System.out.println(oreEnTrop);
+    // } else {
+    // retour += reactions.get(x).findCountOre(
+    // this.entrees.get(x) * (long) Math.ceil((double) quantite / this.quantite),
+    // reactions, oreEnTrop);
+    // }
+    // }
+    //
+    // return retour;
+    // }
 
 }
