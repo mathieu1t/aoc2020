c88c90779250ea22e71d7263ee31d7459210445f
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2020-12-15 09:23:25 +0100

    Day15Refactor

--- src/main/java/fr/insee/adventofcode/days/Day15Refactor.java
+++ src/main/java/fr/insee/adventofcode/days/Day15Refactor.java
@@ -1,19 +1,55 @@
 package fr.insee.adventofcode.days;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Day15Refactor extends Day {
 
-    //private static final String[] puzzle = Utils.getLineString("src/main/resources/12.txt");
+    private static final Long[] puzzle = {
+        1l, 20l, 8l, 12l, 0l, 14l
+    };
 
     @Override
     public String part1() {
-	
-	return String.valueOf("");
+        Map<Long, Long> suite = new HashMap<>();
+        long i = 0;
+        long last = 0;
+        for (Long l : puzzle) {
+            suite.put(l, i + 1); // on veut l'index du dernier nombre parlé donc la clé c'est le nombre
+            i ++ ;
+            last = l;
+        }
+
+        last = getNumber(2020, i, last, suite);
+        return String.valueOf(last);
     }
 
     @Override
     public String part2() {
-	
-	return String.valueOf("");
+        Map<Long, Long> suite = new HashMap<>();
+        long i = 0;
+        long last = 0;
+        for (Long l : puzzle) {
+            suite.put(l, i + 1); // on veut l'index du dernier nombre parlé donc la clé c'est le nombre
+            i ++ ;
+            last = l;
+        }
+
+        last = getNumber(30000000, i, last, suite);
+        return String.valueOf(last);
+    }
+
+    private long getNumber(long n, long i, long last, Map<Long, Long> suite) {
+        while (i < n) {
+            long next = 0;
+            if (suite.containsKey(last)) {
+                next = i - suite.get(last);
+            }
+            suite.put(last, i);
+            i ++ ;
+            last = next;
+        }
+        return last;
     }
 
 }
