efacf03a799bba56ddb58a59d96f976a011246a3
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2020-12-17 10:52:58 +0100

    Day17 part1

--- src/main/java/fr/insee/adventofcode/days/Day17.java
+++ src/main/java/fr/insee/adventofcode/days/Day17.java
@@ -10,126 +10,152 @@ public class Day17 extends Day {
 
     private static final String[][] puzzle = Utils.getLineString("src/main/resources/17.txt", "");
 
-    // Pourquoi c'est plus long??
-
     @Override
     public String part1() {
-	List<Coordinates> activeCoordinates = new ArrayList<>();
+        List<Coordinates> coordinates = new ArrayList<>();
 
-	for (int x = 0; x < puzzle.length; x++) {
-	    for (int y = 0; y < puzzle.length; y++) {
-		if ("#".equals(puzzle[x][y])) {
-		    activeCoordinates.add(new Coordinates(x, y, 0));
-		}
-	    }
-	}
-	for (int n = 0; n < 6; n++) {
-	    for (int x = -(puzzle.length + n) ; x < puzzle.length + n; x++) {
-		for (int y = -(puzzle.length + n); y < puzzle.length + n; y++) {
-		    for (int z = -(puzzle.length + n); z < puzzle.length + n; z++) {
-			List<Coordinates> activeNeightbors = getActiveNeightbors(activeCoordinates, x, y, z);
-			System.out.println(x +" " + y + " " + z + " : " +activeNeightbors);
-			int active = activeNeightbors.size();
-			Coordinates c = new Coordinates(x, y, z);
-			if (activeCoordinates.contains(c) && active != 2 && active != 3) {
-			    activeCoordinates.remove(c);
-			}
-			if (!activeCoordinates.contains(c) && active == 3) {
-			    activeCoordinates.add(c);
-			}
-		    }
-		}
-	    }
-	    System.out.println(activeCoordinates.size());
-	}
-	int count = activeCoordinates.size();
+        for (int x = 0; x < puzzle.length; x ++ ) {
+            for (int y = 0; y < puzzle.length; y ++ ) {
+                if ("#".equals(puzzle[x][y])) {
+                    coordinates.add(new Coordinates(x, y, 0, true, null));
+                }
+                else {
+                    coordinates.add(new Coordinates(x, y, 0, false, null));
+                }
+            }
+        }
 
-//	do {
-//	    String[][][] newGrid = new String[50][50][50];
-//	    
-//	    for (int x = 0; x < grid.length; x++) {
-//		for (int y = 0; y < grid[0].length; y++) {
-//		    for (int z = 0; z < grid[0].length; z++) {
-//		    newGrid[x][y] = changeSeat(grid, grid[x][y], x, y);
-//		}
-//	    }
-//	    if (Arrays.deepEquals(grid, newGrid)) {
-//		break;
-//	    }
-//	    grid = newGrid;
-//	} while (true);
-//	long count = 0;
-//	for (int x = 0; x < grid.length; x++) {
-//	    for (int y = 0; y < grid[0].length; y++) {
-//		if ("#".equals(grid[x][y])) {
-//		    count++;
-//		}
-//	    }
-//	}
-	return String.valueOf(count);
+        for (int n = 0; n < 6; n ++ ) {
+            int minX = getMinX(coordinates);
+            int maxX = getMaxX(coordinates);
+            int minY = getMinY(coordinates);
+            int maxY = getMaxY(coordinates);
+            int minZ = getMinZ(coordinates);
+            int maxZ = getMaxZ(coordinates);
+            for (int x = minX; x <= maxX; x ++ ) {
+                for (int y = minY; y <= maxY; y ++ ) {
+                    for (int z = minZ; z <= maxZ; z ++ ) {
+                        List<Coordinates> activeNeightbors = getActiveNeightbors(coordinates, x, y, z);
+                        long active = activeNeightbors.size();
+                        Coordinates c = findCoordinates(coordinates, x, y, z);
+                        if (c == null) {
+                            c = new Coordinates(x, y, z, false, null);
+                            coordinates.add(c);
+                        }
+                        if (c.active && active != 2l && active != 3l) {
+                            c.activeInNext = false;
+                        }
+                        if ( !c.active && active == 3l) {
+                            c.activeInNext = true;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < coordinates.size(); i ++ ) {
+                Coordinates c = coordinates.get(i);
+                if (c.activeInNext != null && c.activeInNext) {
+                    c.active = true;
+                    c.activeInNext = null;
+                }
+                if (c.activeInNext != null && !c.activeInNext) {
+                    c.active = false;
+                    c.activeInNext = null;
+                }
+            }
+        }
+
+        return String.valueOf(coordinates.stream().filter(c -> c.active != null && c.active).count());
     }
 
-    private List<Coordinates> getActiveNeightbors(List<Coordinates> activeCoordinates, int x, int y, int z) {
-	List<Coordinates> active = new ArrayList<>();
+    private int getMinX(List<Coordinates> coordinates) {
+        return coordinates.stream().mapToInt(c -> c.x).min().getAsInt() - 1;
+    }
 
-	for (int i = -1; i <= 1; i++) {
-	    for (int j = -1; j <= 1; j++) {
-		for (int k = -1; k <= 1; k++) {
-		    if (i == 0 && j == 0 && k == 0) {
-			continue;
-		    }
-		    Coordinates c = new Coordinates(x + i, y + j, z + k);
-		    if (activeCoordinates.contains(c)) {
-			active.add(c);
-		    }
-		}
-	    }
-	}
+    private int getMaxX(List<Coordinates> coordinates) {
+        return coordinates.stream().mapToInt(c -> c.x).max().getAsInt() + 1;
+    }
 
-	return active;
+    private int getMinY(List<Coordinates> coordinates) {
+        return coordinates.stream().mapToInt(c -> c.y).min().getAsInt() - 1;
+    }
+
+    private int getMaxY(List<Coordinates> coordinates) {
+        return coordinates.stream().mapToInt(c -> c.y).max().getAsInt() + 1;
+    }
+
+    private int getMinZ(List<Coordinates> coordinates) {
+        return coordinates.stream().mapToInt(c -> c.z).min().getAsInt() - 1;
+    }
+
+    private int getMaxZ(List<Coordinates> coordinates) {
+        return coordinates.stream().mapToInt(c -> c.z).max().getAsInt() + 1;
+    }
+
+    private Coordinates findCoordinates(List<Coordinates> coordinates, int x, int y, int z) {
+        return coordinates.stream().filter(c -> c.x == x && c.y == y && c.z == z).findFirst().orElse(null);
+    }
+
+    private List<Coordinates> getActiveNeightbors(List<Coordinates> coordinates, int x, int y, int z) {
+        List<Coordinates> active = new ArrayList<>();
+
+        for (int i = -1; i <= 1; i ++ ) {
+            for (int j = -1; j <= 1; j ++ ) {
+                for (int k = -1; k <= 1; k ++ ) {
+                    if (i == 0 && j == 0 && k == 0) {
+                        continue;
+                    }
+                    Coordinates c = findCoordinates(coordinates, x + i, y + j, z + k);
+                    if (c != null && c.active != null && c.active) {
+                        active.add(c);
+                    }
+                }
+            }
+        }
+
+        return active;
     }
 
     @Override
     public String part2() {
 
-	return String.valueOf("");
+        return String.valueOf("");
     }
 
     class Coordinates {
 
-	int x;
-	int y;
-	int z;
+        int x;
+        int y;
+        int z;
+        Boolean active;
+        Boolean activeInNext;
 
-	public Coordinates(int x, int y, int z) {
-	    this.x = x;
-	    this.y = y;
-	    this.z = z;
-	}
+        public Coordinates(int x, int y, int z, Boolean active, Boolean activeInNext) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.active = active;
+            this.activeInNext = activeInNext;
+        }
 
-	@Override
-	public int hashCode() {
-	    return Objects.hash(x, y, z);
-	}
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y, z);
+        }
 
-	@Override
-	public boolean equals(Object obj) {
-	    if (this == obj)
-		return true;
-	    if (obj == null)
-		return false;
-	    if (getClass() != obj.getClass())
-		return false;
-	    Coordinates other = (Coordinates) obj;
-	    return x == other.x && y == other.y && z == other.z;
-	}
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (getClass() != obj.getClass()) return false;
+            Coordinates other = (Coordinates) obj;
+            return x == other.x && y == other.y && z == other.z;
+        }
 
-	@Override
-	public String toString() {
-	    return "Coordinates [x=" + x + ", y=" + y + ", z=" + z + "]";
-	}
-	
-	
+        @Override
+        public String toString() {
+            return "Coordinates [x=" + x + ", y=" + y + ", z=" + z + ", active=" + active + ", activeInNext=" + activeInNext + "]";
+        }
+
     }
 
 }
