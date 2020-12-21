663a40e0bd9462ede99751ea3a6ea840eb7f0880
mat59800 <mat59800@gmail.com>
2020-12-21 09:49:33 +0100

    Day21

--- src/main/java/fr/insee/adventofcode/days/Day21.java
+++ src/main/java/fr/insee/adventofcode/days/Day21.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 import fr.insee.adventofcode.utils.Utils;
 
@@ -13,12 +14,13 @@ public class Day21 extends Day {
 
     private static final List<String> puzzle = Utils.getLineStringList("src/main/resources/21.txt");
 
+    private Map<String, Set<String>> allergensMap = new TreeMap<>();
+
     @Override
-    public String part1() {	
-	Map<String, Set<String>> allergensMap = new HashMap<>();
-	Map<String,Integer> ingredientsMap = new HashMap<>();
+    public String part1() {
+	Map<String, Integer> ingredientsMap = new HashMap<>();
 	for (String line : puzzle) {
-	    String[] lineTab = line.replace(")","").split("\\(contains ");
+	    String[] lineTab = line.replace(")", "").split("\\(contains ");
 	    String[] ingredientsTab = lineTab[0].split(" ");
 	    String[] allergensTab = lineTab[1].split(", ");
 	    List<String> ingredients = Arrays.asList(ingredientsTab);
@@ -26,7 +28,7 @@ public class Day21 extends Day {
 		if (!ingredientsMap.containsKey(s)) {
 		    ingredientsMap.put(s, 1);
 		} else {
-		    ingredientsMap.put(s, ingredientsMap.get(s)+1);
+		    ingredientsMap.put(s, ingredientsMap.get(s) + 1);
 		}
 	    }
 	    List<String> allergens = Arrays.asList(allergensTab);
@@ -34,8 +36,8 @@ public class Day21 extends Day {
 		if (!allergensMap.containsKey(allergen)) {
 		    allergensMap.put(allergen, new HashSet<>(ingredients));
 		} else {
-		   Set<String> ingredientsInAllergen = allergensMap.get(allergen);
-		   ingredientsInAllergen.retainAll(ingredients);
+		    Set<String> ingredientsInAllergen = allergensMap.get(allergen);
+		    ingredientsInAllergen.retainAll(ingredients);
 		}
 	    }
 	}
@@ -51,8 +53,30 @@ public class Day21 extends Day {
 
     @Override
     public String part2() {
-
-	return String.valueOf("");
+	int sum = allergensMap.entrySet().stream().mapToInt(a -> a.getValue().size()).sum();
+	while (sum != allergensMap.size()) {
+	    for (String all : allergensMap.keySet()) {
+		Set<String> set = allergensMap.get(all);
+		if (set.size() == 1) {
+		    String string = set.stream().findFirst().get();
+		    for (String all2 : allergensMap.keySet()) {
+			if (!all.equals(all2)) {
+			    Set<String> set2 = allergensMap.get(all2);
+			    set2.remove(string);
+			}
+		    }
+		}
+	    }
+	    sum = allergensMap.entrySet().stream().mapToInt(a -> a.getValue().size()).sum();
+	}
+	StringBuffer sb = new StringBuffer();
+	for (String all : allergensMap.keySet()) {
+	    Set<String> set = allergensMap.get(all);
+	    String s = set.stream().findFirst().get();
+	    sb.append(",");
+	    sb.append(s);
+	}
+	return sb.substring(1);
     }
 
 }
