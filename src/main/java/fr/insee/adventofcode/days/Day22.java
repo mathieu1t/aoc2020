4f0358365d3f1bb013282017118474f61dc3d577
mat59800 <mat59800@gmail.com>
2020-12-22 06:28:16 +0100

    Day22 part 1

--- src/main/java/fr/insee/adventofcode/days/Day22.java
+++ src/main/java/fr/insee/adventofcode/days/Day22.java
@@ -1,13 +1,43 @@
 package fr.insee.adventofcode.days;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
+import fr.insee.adventofcode.utils.Utils;
+
 public class Day22 extends Day {
 
-    //private static final String[] puzzle = Utils.getLineString("src/main/resources/12.txt");
+    private static final List<Integer> puzzle1 = Utils.getLineIntegerList("src/main/resources/22_player1.txt");
+    private static final List<Integer> puzzle2 = Utils.getLineIntegerList("src/main/resources/22_player2.txt");
 
     @Override
     public String part1() {
+	Deque<Integer> player1 = new ArrayDeque<>(puzzle1);
+	Deque<Integer> player2 = new ArrayDeque<>(puzzle2);
+	int j = 0;
+	while (player1.size()!=0 && player2.size()!=0) {
+	    System.out.println(j++);
+	    Integer card1 = player1.pollFirst();
+	    Integer card2 = player2.pollFirst();
+	    if (card1 > card2) {
+		player1.addLast(card1);
+		player1.addLast(card2);
+	    } else {
+		player2.addLast(card2);
+		player2.addLast(card1);
+	    }
+	}
+	int i = 1;
+	int count = 0;
+	Deque<Integer> gagnant = player1.size() == 0 ? player2:player1;
+	while (gagnant.size() != 0) {
+	    count = count + i * gagnant.pollLast();
+	    i++;
+	}
 	
-	return String.valueOf("");
+
+	return String.valueOf(count);
     }
 
     @Override
