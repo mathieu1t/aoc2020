fc4d38f8aca4ed84cf17fa3e5e1240bcdf7d790f
mat59800 <mat59800@gmail.com>
2020-12-22 09:12:06 +0100

    Day22

--- src/main/java/fr/insee/adventofcode/days/Day22.java
+++ src/main/java/fr/insee/adventofcode/days/Day22.java
@@ -2,7 +2,10 @@ package fr.insee.adventofcode.days;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import fr.insee.adventofcode.utils.Utils;
 
@@ -15,9 +18,7 @@ public class Day22 extends Day {
     public String part1() {
 	Deque<Integer> player1 = new ArrayDeque<>(puzzle1);
 	Deque<Integer> player2 = new ArrayDeque<>(puzzle2);
-	int j = 0;
-	while (player1.size()!=0 && player2.size()!=0) {
-	    System.out.println(j++);
+	while (player1.size() != 0 && player2.size() != 0) {
 	    Integer card1 = player1.pollFirst();
 	    Integer card2 = player2.pollFirst();
 	    if (card1 > card2) {
@@ -30,20 +31,73 @@ public class Day22 extends Day {
 	}
 	int i = 1;
 	int count = 0;
-	Deque<Integer> gagnant = player1.size() == 0 ? player2:player1;
+	Deque<Integer> gagnant = player1.size() == 0 ? player2 : player1;
 	while (gagnant.size() != 0) {
 	    count = count + i * gagnant.pollLast();
 	    i++;
 	}
-	
 
 	return String.valueOf(count);
     }
 
     @Override
     public String part2() {
-	
-	return String.valueOf("");
+	Deque<Integer> player1 = new ArrayDeque<>(puzzle1);
+	Deque<Integer> player2 = new ArrayDeque<>(puzzle2);
+	Integer gagnant = game(player1, player2);
+	int i = 1;
+	int count = 0;
+	Deque<Integer> g = gagnant == 1 ? player1 : player2;
+	while (g.size() != 0) {
+	    count = count + i * g.pollLast();
+	    i++;
+	}
+
+	return String.valueOf(count);
+    }
+
+    private Integer game(Deque<Integer> player1, Deque<Integer> player2) {
+	Set<String> roundsPlayer = new HashSet<>();
+	while (player1.size() != 0 && player2.size() != 0) {
+	    if (!roundsPlayer.add((player1.toString() + player2.toString()))) {
+		return 1;
+	    }
+	    Integer card1 = player1.pollFirst();
+	    Integer card2 = player2.pollFirst();
+	    if (card1 <= player1.size() && card2 <= player2.size()) {
+		Deque<Integer> player1Clone = new ArrayDeque<>(player1);
+		Deque<Integer> player2Clone = new ArrayDeque<>(player2);
+		while (player1Clone.size() > card1) {
+		    player1Clone.removeLast();
+		}
+		while (player2Clone.size() > card2) {
+		    player2Clone.removeLast();
+		}
+		Integer gagnant = game(player1Clone, player2Clone);
+
+		if (gagnant == 1) {
+		    player1.addLast(card1);
+		    player1.addLast(card2);
+		} else {
+		    player2.addLast(card2);
+		    player2.addLast(card1);
+		}
+	    } else {
+		if (card1 > card2) {
+		    player1.addLast(card1);
+		    player1.addLast(card2);
+		} else {
+		    player2.addLast(card2);
+		    player2.addLast(card1);
+		}
+	    }
+	}
+	if (player1.size() == 0) {
+	    return 2;
+	} else {
+	    return 1;
+	}
+
     }
 
 }
